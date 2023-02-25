3f41698834d4c99f15cc90d5325f59fe02b34172
bobu <113674920+ElCreaturoM50S@users.noreply.github.com>
2023-02-25 19:20:53 +0100

    roblox update

--- main.py
+++ main.py
@@ -2,56 +2,110 @@ import sys
 import requests
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+import textwrap
 
-from PIL import Image
+from io import BytesIO
+from PIL import Image,ImageQt,ImageDraw,ImageFont
 #
 
-class Main(QDialog):
+class ImageWindow(QWidget):
+    def __init__(self,imagelink,toptext,bottomtext):
+        super().__init__()
+        print(imagelink)
+        self.setWindowTitle("Image")
+
+
+        #!!!WE DONT TALK ABOUT THIS!!!
+        #!!!WE DONT TALK ABOUT THIS!!!
+        #!!!WE DONT TALK ABOUT THIS!!!
+        req = requests.get(imagelink)
+        imgbytes = req.content
+        imgbytesconvert = BytesIO(imgbytes)
+        img = Image.open(imgbytesconvert)
+
+
+        #save space
+        ##add text
+        draw = ImageDraw.Draw(img)
+        print(img.size[1])
+        font = ImageFont.truetype(font="./impact/impact.ttf", size=int(img.size[0]/10))
+        fontWidth, fontHeight = font.getsize("A")
+        charsPerLine = img.size[1] // fontWidth
+        toplines = textwrap.wrap(toptext, width=charsPerLine)
+        bottomlines = textwrap.wrap(bottomtext, width=charsPerLine)
+
+        y = 10 
+        for line in toplines:
+            lineWidth, lineHeight = font.getsize(line)
+            x = (img.size[1] - lineWidth)/2
+            draw.text((x,y), line, fill='white', font=font, stroke_width=4, stroke_fill='black')
+            y += lineHeight
+        
+        y = img.size[0] - fontHeight * len(bottomlines) - 15
+        for line in bottomlines:
+            lineWidth, lineHeight = font.getsize(line)
+            x = (img.size[1] - lineWidth)/2
+            draw.text((x,y), line, fill='white', font=font, stroke_width=4, stroke_fill='black')
+            y += lineHeight
+        #save space
+
+
+        qtimg = ImageQt.toqpixmap(img)
+        #!!!WE DONT TALK ABOUT THIS!!!
+        #!!!WE DONT TALK ABOUT THIS!!!
+        #!!!WE DONT TALK ABOUT THIS!!!
+        
+
+        #label and size
+        self.imageLabel = QLabel(self)
+        self.image = QPixmap(qtimg)
+        self.imageLabel.setPixmap(self.image)
+        self.resize(img.size[1],img.size[0])
+        
+
+class Main(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Cos tam nie apie")
-        self.resize(600,400)
+        self.setWindowTitle("")
+        self.resize(300,200)
         
         self.lyt_main = QHBoxLayout()
-        self.lyt_left = QHBoxLayout()
-        self.lyt_right = QHBoxLayout()
         self.lyt_form = QFormLayout()
 
+        self.btn1 = QPushButton("Generuj obrazek")
+        self.lineedit1 = QLineEdit("34485603")
+        self.lineedit1.setPlaceholderText("Enter player id")
+        self.lineedit2 = QLineEdit("Teskt górny")
+        self.lineedit2.setPlaceholderText("Top text")
+        self.lineedit3 = QLineEdit("Tekst dolny")
+        self.lineedit3.setPlaceholderText("Bottom text")
+        self.lbl1 = QLabel("Generuj obrazek")
+        self.funnyimage = None
 
-        def pobierz_dane(self):
-            res = requests.get(url="http://api.open-notify.org/iss-now.json")
+        #tu jest api  o tu
+        #               |
+        #               |
+        #               v
+        #https://thumbnails.roblox.com/docs/index.html
+        urltemplate = "https://thumbnails.roblox.com/v1/users/avatar-headshot?"
+        def generujObrazek():
+            urlother = f"userIds={self.lineedit1.text()}&size=420x420&format=Png&isCircular=false"
+            fullurl = (urltemplate+urlother)
+            res = requests.get(url=fullurl)
             res.raise_for_status()
-
             dane = res.json()
-            print(dane)
-            lat = dane['iss_position']['latitude']
-            long = dane['iss_position']['longitude']
-            self.lbl1.setText(f" {lat}, {long}")
+            if len(dane['data'][0]) < 2:
+                return False 
+            self.funnyimage = ImageWindow(dane['data'][0]['imageUrl'],self.lineedit2.text(),self.lineedit3.text())
+            self.funnyimage.show()
 
-        def nowe_dane(self):
-            res = requests.get(url="htpp://swapi.dev/api/people/1")
-            data = res.json()
-            print(data)
-            name = data['name']
-            height = data["height"]
-            mass = data["mass"]       
-
-
-        self.btn1 = QPushButton("daj dane stacji")
-        self.btn1.resize(100,50)
-        self.btn2 = QPushButton("machnt")
-        self.lyt_left.addWidget(self.btn2)
-        self.lyt_left.addWidget(self.btn1)
-
-        self.lbl1 = QLabel("to bede dane stacji")
-        self.lbl2 = QLabel("tu tu pusto jest")
-        self.lyt_right.addWidget(self.lbl1)
-        self.lyt_left.addWidget(self.lbl2)
-
-        self.lyt_main.addLayout(self.lyt_left)
-        self.lyt_main.addLayout(self.lyt_right)
-
-        self.btn1.clicked.connect(nowe_dane)
+        self.btn1.clicked.connect(generujObrazek)
+        
+        self.lyt_form.addWidget(self.lineedit1)
+        self.lyt_form.addWidget(self.lineedit2)
+        self.lyt_form.addWidget(self.lineedit3)
+        self.lyt_form.addWidget(self.btn1)
+        self.lyt_main.addLayout(self.lyt_form)
 
         self.setLayout(self.lyt_main)
 
@@ -60,3 +114,6 @@ if __name__ == '__main__':
     okno = Main()
     okno.show()
     sys.exit(app.exec_())
+
+
+    
