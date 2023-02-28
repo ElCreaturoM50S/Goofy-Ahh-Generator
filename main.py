370dd3adc39f55e86dc3df574d44972a29522b5c
ElCreaturoM50S <113674920+ElCreaturoM50S@users.noreply.github.com>
2023-02-28 13:44:21 +0100

    więcej wszystkiego

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from PIL import Image,ImageQt,ImageDraw,ImageFont
 class ImageWindow(QWidget):
     def __init__(self,imagelink,toptext,bottomtext):
         super().__init__()
-        print(imagelink)
         self.setWindowTitle("Image")
 
 
@@ -27,7 +26,6 @@ class ImageWindow(QWidget):
         #save space
         ##add text
         draw = ImageDraw.Draw(img)
-        print(img.size[1])
         font = ImageFont.truetype(font="./impact/impact.ttf", size=int(img.size[0]/10))
         fontWidth, fontHeight = font.getsize("A")
         charsPerLine = img.size[1] // fontWidth
@@ -61,6 +59,7 @@ class ImageWindow(QWidget):
         self.image = QPixmap(qtimg)
         self.imageLabel.setPixmap(self.image)
         self.resize(img.size[1],img.size[0])
+        #self.resize(1920,1080)
         
 
 class Main(QWidget):
@@ -68,9 +67,11 @@ class Main(QWidget):
         super().__init__()
         self.setWindowTitle("")
         self.resize(300,200)
-        
+
         self.lyt_main = QHBoxLayout()
         self.lyt_form = QFormLayout()
+
+        self.msg = None
 
         self.btn1 = QPushButton("Generuj obrazek")
         self.lineedit1 = QLineEdit("34485603")
@@ -79,6 +80,29 @@ class Main(QWidget):
         self.lineedit2.setPlaceholderText("Top text")
         self.lineedit3 = QLineEdit("Tekst dolny")
         self.lineedit3.setPlaceholderText("Bottom text")
+
+        self.combobox1 = QComboBox()
+        self.combobox1.addItems([
+            '48x48',
+            '50x50',
+            '60x60',
+            '75x75',
+            '100x100',
+            '110x110',
+            '150x150',
+            '180x180',
+            '352x352',
+            '420x420',
+            '720x720'
+            ])
+        
+        self.combobox2 = QComboBox()
+        self.combobox2.addItems([
+            'avatar-headshot',
+            'avatar-bust',
+            'avatar',
+        ])
+
         self.lbl1 = QLabel("Generuj obrazek")
         self.funnyimage = None
 
@@ -87,23 +111,40 @@ class Main(QWidget):
         #               |
         #               v
         #https://thumbnails.roblox.com/docs/index.html
-        urltemplate = "https://thumbnails.roblox.com/v1/users/avatar-headshot?"
-        def generujObrazek():
-            urlother = f"userIds={self.lineedit1.text()}&size=420x420&format=Png&isCircular=false"
-            fullurl = (urltemplate+urlother)
-            res = requests.get(url=fullurl)
-            res.raise_for_status()
-            dane = res.json()
-            if len(dane['data'][0]) < 2:
-                return False 
-            self.funnyimage = ImageWindow(dane['data'][0]['imageUrl'],self.lineedit2.text(),self.lineedit3.text())
-            self.funnyimage.show()
 
+        def errorFunction(textcvos):
+            self.msg = QMessageBox()
+            self.msg.setIcon(QMessageBox.Critical)
+            self.msg.setText("Błąd lmao")
+            self.msg.setInformativeText(textcvos)
+            self.msg.setWindowTitle("Error")
+            self.msg.show()
+
+        def generujObrazek():
+            urltemplate = f"https://thumbnails.roblox.com/v1/users/{self.combobox2.currentText()}?"
+            urlother = f"userIds={self.lineedit1.text()}&size={self.combobox1.currentText()}&format=Png&isCircular=false"
+            fullurl = (urltemplate+urlother)
+            
+            res = None
+            try:
+                res = requests.get(url=fullurl)
+                res.raise_for_status()
+                dane = res.json()
+                if len(dane['data'][0]) < 2:
+                    return False 
+                self.funnyimage = ImageWindow(dane['data'][0]['imageUrl'],self.lineedit2.text(),self.lineedit3.text())
+                self.funnyimage.show()
+            except:
+                errorFunction("coś jest nie tak")
+
+            
         self.btn1.clicked.connect(generujObrazek)
         
         self.lyt_form.addWidget(self.lineedit1)
         self.lyt_form.addWidget(self.lineedit2)
         self.lyt_form.addWidget(self.lineedit3)
+        self.lyt_form.addWidget(self.combobox1)
+        self.lyt_form.addWidget(self.combobox2)
         self.lyt_form.addWidget(self.btn1)
         self.lyt_main.addLayout(self.lyt_form)
 
